83ac2728b2aace63802f5977a64dbe6029c1d9b3
Colton Watkins <watkincw@gmail.com>
2025-02-10 16:03:18 -0600

    <div id="popups" />

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Component, ParentProps } from "solid-js";
 const App: Component<ParentProps> = (props) => {
   return (
     <>
+      <div id="popups" />
       {props.children}
     </>
-  )
+  );
 };
 
 export default App;
