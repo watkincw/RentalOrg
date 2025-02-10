e172a8a5a734c3ce32cea8e9cb1b531a9481bc2a
Colton Watkins <watkincw@gmail.com>
2025-02-10 11:47:36 -0600

    Pushing for context on comment on lesson "25. Routing Changes! Important."

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Component } from "solid-js";
+// screens
 import HomeScreen from "./screens/Home";
 import LoginScreen from "./screens/Login";
 import RegisterScreen from "./screens/Register";
