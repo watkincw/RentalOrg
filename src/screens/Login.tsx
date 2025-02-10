8db2986af94e2490d0f9a41ee2a6e2e53cf40d3b
Colton Watkins <watkincw@gmail.com>
2025-02-10 10:51:17 -0600

    Created LoginScreen and RegisterScreen

---
+++ src/screens/Login.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "../components/layouts/Main";
+
+const LoginScreen = () => {
+  return (
+    <MainLayout>
+        Hello I am LoginScreen
+    </MainLayout>
+  ) 
+};
+
+export default LoginScreen;
