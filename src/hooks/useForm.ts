836bab4ec47f18cbf933407f60318bfe324b415f
Colton Watkins <watkincw@gmail.com>
2025-02-13 14:12:09 -0600

    66. Simple Validation

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -5,6 +5,7 @@ import { Form, GliderInputEvent, SubmitCallback } from "../types/Form";
 
 const useForm = <T extends Form>(initialForm: T) => {
   const [form, setForm] = createStore(initialForm);
+  const [errors, setErrors] = createStore<Form>();
 
   const handleInput = (e: GliderInputEvent) => {
     const { name, value } = e.currentTarget;
@@ -20,13 +21,24 @@ const useForm = <T extends Form>(initialForm: T) => {
   const validate = (ref: HTMLInputElement, accessor: Accessor<number>) => {
     const value = accessor();
 
-    ref.onblur = () => {
-      console.log("OnBlur");
-    };
+    ref.onblur = checkValidity(ref);
+  };
 
-    ref.oninput = () => {
-      console.log("OnInput");
-    };
+  const validator = (ref: HTMLInputElement) => {
+    return false;
+  };
+
+  const checkValidity = (ref: HTMLInputElement) => () => {
+    const message = "Error error error";
+    const isValid = validator(ref);
+
+    if (!isValid) {
+      setErrors(ref.name, message);
+    } else {
+      setErrors(ref.name, "");
+    }
+
+    console.log(JSON.stringify(errors));
   };
 
   return {
