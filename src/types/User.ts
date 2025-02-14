c7cd60e9aacec52fb7f822b36f900959e6df2fd2
Colton Watkins <watkincw@gmail.com>
2025-02-14 12:48:11 -0600

    89. Extend User Type

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,4 +1,12 @@
+import { DocumentReference } from "firebase/firestore";
+
 export interface User {
+  uid: string;
+  fullName: string;
   nickName: string;
   avatar: string;
+  followers: DocumentReference[];
+  following: DocumentReference[];
+  followersCount: number;
+  follingCount: number;
 }
