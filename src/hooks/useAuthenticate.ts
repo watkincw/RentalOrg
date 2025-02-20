6c08dd0d9b3c8cc5384fb78708b55813f4c61435
Colton Watkins <watkincw@gmail.com>
2025-02-20 12:44:16 -0600

    111. Display Snackbar on Authentication; Section 15 END

--- src/hooks/useAuthenticate.ts
+++ src/hooks/useAuthenticate.ts
@@ -1,19 +1,24 @@
 import { FirebaseError } from "firebase/app";
-// api
 import { createSignal } from "solid-js";
+// api
 import { authenticate, AuthType } from "../api/auth";
 // types
 import { AuthForm } from "../types/Form";
+// context
+import { useUIDispatch } from "../context/ui";
 
 const useAuthenticate = (authType: AuthType) => {
   const [loading, setLoading] = createSignal(false);
+  const { addSnackbar } = useUIDispatch();
 
   const authUser = async (form: AuthForm) => {
     setLoading(true);
     try {
       await authenticate(form, authType);
+      addSnackbar({ message: "Logged in Successful", type: "success" });
     } catch (e) {
       const message = (e as FirebaseError).message;
+      addSnackbar({ message, type: "error" });
       console.log(message);
     } finally {
       setLoading(false);
