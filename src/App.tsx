eb7b6d85b31e4b6a50ee001973d5eb9f2db0e3a6
Colton Watkins <watkincw@gmail.com>
2025-02-20 10:52:57 -0600

    102. Snackbar start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Component, ParentProps } from "solid-js";
+// componenmts/snackbar
+import SnackbarContainer from "./components/snackbar/Container";
 
 const App: Component<ParentProps> = (props) => {
   return (
     <>
       <div id="popups" />
+      <SnackbarContainer />
       {props.children}
     </>
   );
