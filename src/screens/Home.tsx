d0601ebb3c74a9528c24b179ff818cfde62dea4b
Colton Watkins <watkincw@gmail.com>
2025-02-25 15:36:19 -0600

    156. Show more glides button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
-import { Component, For, onCleanup, onMount } from "solid-js";
+import { Component, onCleanup, onMount, Show } from "solid-js";
+import { Portal } from "solid-js/web";
 // components
 import MainLayout from "../components/layouts/Main";
 import Messenger from "../components/utils/Messenger";
@@ -28,6 +29,20 @@ const HomeScreen: Component = () => {
     <MainLayout pageTitle="Home">
       <Messenger onGlideAdded={addGlide} />
       <div class="h-px bg-gray-700 my-1" />
+      <Show when={store.newGlides.length >= 3}>
+        <Portal>
+          <div class="fixed top-2 z-100 left-2/4 -translate-x-1/2">
+            <button
+              type="button"
+              class="disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
+            >
+              <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
+                <span>Read New Glides</span>
+              </div>
+            </button>
+          </div>
+        </Portal>
+      </Show>
       <PaginatedGlides
         page={page}
         pages={store.pages}
