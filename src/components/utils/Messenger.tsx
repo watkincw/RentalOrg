6fe630381e7673360a7a34af87d2da0acaff723f
Colton Watkins <watkincw@gmail.com>
2025-02-25 14:16:50 -0600

    150. Disable send button Section 20: Follow User DONE

--- src/components/utils/Messenger.tsx
+++ src/components/utils/Messenger.tsx
@@ -16,7 +16,9 @@ type Props = {
 
 const Messenger: Component<Props> = (props) => {
   const { user } = useAuthState()!;
-  const { handleInput, handleSubmit, form } = useMessenger();
+  const { handleInput, handleSubmit, form, loading } = useMessenger();
+
+  const sendDisabled = () => loading() || form.content === "";
 
   const autoSize = (e: GliderInputEvent) => {
     const el = e.currentTarget;
@@ -66,6 +68,7 @@ const Messenger: Component<Props> = (props) => {
           </div>
           <div class="flex-it w-32 mt-3 cursor-pointer">
             <button
+              disabled={sendDisabled()}
               onClick={async () => {
                 const glide = await handleSubmit();
                 props.onGlideAdded(glide);
