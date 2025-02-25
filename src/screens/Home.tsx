f88e216ff5ec0a20f2cce98a893c5c0287a9deef
Colton Watkins <watkincw@gmail.com>
2025-02-25 15:58:19 -0600

    157. Wrapper button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { Portal } from "solid-js/web";
 // components
 import MainLayout from "../components/layouts/Main";
 import Messenger from "../components/utils/Messenger";
+import Button from "../components/utils/Button";
 import PaginatedGlides from "../components/glides/PaginatedGlides";
 // hooks
 import useGlides from "../hooks/useGlides";
@@ -32,14 +33,9 @@ const HomeScreen: Component = () => {
       <Show when={store.newGlides.length >= 3}>
         <Portal>
           <div class="fixed top-2 z-100 left-2/4 -translate-x-1/2">
-            <button
-              type="button"
-              class="disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
-            >
-              <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-                <span>Read New Glides</span>
-              </div>
-            </button>
+            <Button onClick={() => alert("Hi there")}>
+              <span>Display New Glides</span>
+            </Button>
           </div>
         </Portal>
       </Show>
