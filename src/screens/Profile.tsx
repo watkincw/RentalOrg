ed63599115eaf014a2b2d44cc5604978d87239ad
Colton Watkins <watkincw@gmail.com>
2025-02-25 11:47:06 -0600

    140. Loader while loading users

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -6,9 +6,10 @@ import MainLayout from "../components/layouts/Main";
 import { useAuthState } from "../context/auth";
 // hooks
 import useUsers from "../hooks/useUsers";
+import { CenteredDataLoader } from "../components/utils/DataLoader";
 
 const ProfileScreen: Component = () => {
-  const { users } = useUsers();
+  const { users, loading } = useUsers();
   const authState = useAuthState()!;
 
   return (
@@ -33,41 +34,45 @@ const ProfileScreen: Component = () => {
             </div>
           </div>
         </div>
-        <For each={users()}>
-          {(user) => (
-            <div class="flex-it p-4">
-              <div class="flex-it flex-row">
-                <div class="flex-it mr-4">
-                  <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
-                    <img
-                      class="rounded-full"
-                      src={authState.user?.avatar || defaultPic}></img>
-                  </div>
-                </div>
-                <article class="flex-it flex-grow flex-shrink">
-                  <div class="flex-it justify-center flex-grow mb-1">
-                    <div class="flex-it justify-between flex-row w-full">
-                      <div class="flex-it justify-center">
-                        <span class="font-bold">{user.userName}</span>
-                      </div>
-                      <div class="flex-it w-32 mt-3 cursor-pointer">
-                        <button
-                          type="button"
-                          class="
-                              disabled:cursor-not-allowed disabled:bg-gray-400
-                              bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200">
-                          <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
-                            <span>Follow</span>
-                          </div>
-                        </button>
-                      </div>
+        <Show
+          when={!loading()}
+          fallback={<CenteredDataLoader />}>
+          <For each={users()}>
+            {(user) => (
+              <div class="flex-it p-4">
+                <div class="flex-it flex-row">
+                  <div class="flex-it mr-4">
+                    <div class="w-12 h-12 overflow-visible cursor-pointer transition duration-200 hover:opacity-80">
+                      <img
+                        class="rounded-full"
+                        src={authState.user?.avatar || defaultPic}></img>
                     </div>
                   </div>
-                </article>
+                  <article class="flex-it flex-grow flex-shrink">
+                    <div class="flex-it justify-center flex-grow mb-1">
+                      <div class="flex-it justify-between flex-row w-full">
+                        <div class="flex-it justify-center">
+                          <span class="font-bold">{user.userName}</span>
+                        </div>
+                        <div class="flex-it w-32 mt-3 cursor-pointer">
+                          <button
+                            type="button"
+                            class="
+                              disabled:cursor-not-allowed disabled:bg-gray-400
+                              bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200">
+                            <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
+                              <span>Follow</span>
+                            </div>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                </div>
               </div>
-            </div>
-          )}
-        </For>
+            )}
+          </For>
+        </Show>
       </div>
     </MainLayout>
   );
