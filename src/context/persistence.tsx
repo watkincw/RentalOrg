e9cc761b285fa05ad51a65c0c780508ba44a352e
Colton Watkins <watkincw@gmail.com>
2025-03-06 11:12:20 -0600

    spacing

--- src/context/persistence.tsx
+++ src/context/persistence.tsx
@@ -1,38 +1,37 @@
-import { createContext,  ParentComponent, useContext } from "solid-js"
-import { createStore, produce } from "solid-js/store"
+import { createContext, ParentComponent, useContext } from "solid-js";
+import { createStore, produce } from "solid-js/store";
 
-type PersistenceStore = {[key: string]: any}
+type PersistenceStore = { [key: string]: any };
 
 type PersistenceContextType = {
-    getValue: (key: string) => any
-    setValue: (key: string, value: any) => void
-}
+  getValue: (key: string) => any;
+  setValue: (key: string, value: any) => void;
+};
 
-const PersistenceContext = createContext<PersistenceContextType>()
+const PersistenceContext = createContext<PersistenceContextType>();
 
 const PersistenceProvider: ParentComponent = (props) => {
-    const [store, setStore] = createStore<PersistenceStore>()
+  const [store, setStore] = createStore<PersistenceStore>();
 
-    const setValue = (key: string, value: any) => {
-        setStore(produce(store => {
-            store[key] = value
-        }))
-        
-    }
-    
-    const getValue = (key: string) => {
-        return store[key]
+  const setValue = (key: string, value: any) => {
+    setStore(
+      produce((store) => {
+        store[key] = value;
+      })
+    );
+  };
 
-    }
+  const getValue = (key: string) => {
+    return store[key];
+  };
 
-    return (
-        <PersistenceContext.Provider value={{getValue, setValue}}>
-            {props.children}
-        </PersistenceContext.Provider>
+  return (
+    <PersistenceContext.Provider value={{ getValue, setValue }}>
+      {props.children}
+    </PersistenceContext.Provider>
+  );
+};
 
-    )
-}
+export const usePersistence = () => useContext(PersistenceContext);
 
-export const usePersistence = () => useContext(PersistenceContext)
-
-export default PersistenceProvider
+export default PersistenceProvider;
