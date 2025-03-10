68edbc0f1c8885673504b2c1b0acb76523eb5d5a
Colton Watkins <watkincw@gmail.com>
2025-03-10 11:37:04 -0500

    testing Vercel with new branch creation

--- src/types/User.ts
+++ src/types/User.ts
@@ -7,7 +7,7 @@ export interface User {
   email: string;
   avatar: string;
   followers: DocumentReference[];
-  following: DocumentReference[];
   followersCount: number;
+  following: DocumentReference[];
   followingCount: number;
 }
